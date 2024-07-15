797f29072bcbe86dd656d63701eef26e25290111
amir786h <amirhusain3737@gmail.com>
2024-07-15 22:43:53 +0530

    SignIn UI changes

--- app/auth/sign-in/index.js
+++ app/auth/sign-in/index.js
@@ -1,10 +1,21 @@
 import { View, Text } from 'react-native'
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useNavigation } from 'expo-router';
 
 export default function Login() {
+  const navigation = useNavigation();
+
+  useEffect(() => {
+    navigation.setOptions({
+      headerShown: false
+    })
+  }, []);
+
   return (
-    <View>
-      <Text>Login</Text>
+    <View style={{
+      padding: 25
+    }}>
+      <Text>Let's Sign You In</Text>
     </View>
   )
 }
