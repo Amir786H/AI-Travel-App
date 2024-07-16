ef0dc96ef58e53281c79872919f210d115f87715
amir786h <amirhusain3737@gmail.com>
2024-07-16 18:15:17 +0530

    Sign-up UI added

--- app/auth/sign-in/index.js
+++ app/auth/sign-in/index.js
@@ -1,10 +1,11 @@
-import { View, Text, TextInput, StyleSheet } from "react-native";
+import { View, Text, TextInput, StyleSheet, Pressable } from "react-native";
 import React, { useEffect } from "react";
-import { useNavigation } from "expo-router";
+import { useNavigation, useRouter } from "expo-router";
 import { Colors } from "@/constants/Colors";
 
 export default function Login() {
   const navigation = useNavigation();
+  const router = useRouter();
 
   useEffect(() => {
     navigation.setOptions({
@@ -46,15 +47,49 @@ export default function Login() {
       </Text>
 
       {/* Email */}
-      <View style={{ marginTop: 30}}>
-        <Text style={{ fontFamily: "outfit"}}>Email</Text>
+      <View style={{ marginTop: 30 }}>
+        <Text style={{ fontFamily: "outfit" }}>Email</Text>
         <TextInput style={styles.input} placeholder="Enter your email" />
       </View>
       {/* Password */}
-      <View style={{ marginTop: 30}}>
-        <Text style={{ fontFamily: "outfit"}}>Password</Text>
-        <TextInput style={styles.input} placeholder="Enter your password" />
+      <View style={{ marginTop: 20 }}>
+        <Text style={{ fontFamily: "outfit" }}>Password</Text>
+        <TextInput
+          style={styles.input}
+          secureTextEntry={true}
+          placeholder="Enter your password"
+        />
       </View>
+
+      {/* Sign-In button */}
+      <View
+        style={{
+          padding: 18,
+          backgroundColor: Colors.PRIMARY,
+          borderRadius: 15,
+          marginTop: 40,
+        }}
+      >
+        <Text style={{ color: Colors.WHITE, textAlign: "center" }}>
+          Sign In
+        </Text>
+      </View>
+      {/* Create Account Button */}
+      <Pressable onPress={()=> router.replace('/auth/sign-up')}>
+        <View
+          style={{
+            padding: 18,
+            backgroundColor: Colors.WHITE,
+            borderRadius: 15,
+            marginTop: 20,
+            borderWidth: 1,
+          }}
+        >
+          <Text style={{ color: Colors.PRIMARY, textAlign: "center" }}>
+            Create Account
+          </Text>
+        </View>
+      </Pressable>
     </View>
   );
 }
@@ -65,6 +100,6 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     borderRadius: 15,
     borderColor: Colors.GRAY,
-    fontFamily: "outfit"
-  }
-})
+    fontFamily: "outfit",
+  },
+});
--- app/auth/sign-up/index.js
+++ app/auth/sign-up/index.js
@@ -1,10 +1,27 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text } from "react-native";
+import React, { useEffect } from "react";
+import { useNavigation, useRouter } from "expo-router";
 
 export default function SignUp() {
+  const navigation = useNavigation();
+  const router = useRouter();
+
+  useEffect(() => {
+    navigation.setOptions({
+      headerShown: false,
+    });
+  }, []);
+
   return (
-    <View>
-      <Text>SignUp</Text>
+    <View
+      style={{
+        padding: 25,
+        paddingTop: 40,
+      }}
+    >
+      <Text style={{ fontFamily: "outfit-bold", fontSize: 26 }}>
+        Create New Account
+      </Text>
     </View>
-  )
+  );
 }
